cbb05a22cc5999f4c227fcbee8f6bc1008fc6341
harkirola75way-del <harkirola.75way@gmail.com>
2025-09-22 16:33:19 +0530

    Add files via upload

--- src/app/api/report-gen/route.js
+++ src/app/api/report-gen/route.js
@@ -1,5 +1,5 @@
-import { chromium as playwright } from "playwright";
-import Chromium from "chrome-aws-lambda";
+import { chromium as playwright } from "playwright-core";
+import Chromium from "@sparticuz/chromium";
 
 export async function POST() {
   try {
