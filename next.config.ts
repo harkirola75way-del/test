3315e0e52f63c827a56b3052051ac7b2f58863a5
harkirola75way-del <harkirola.75way@gmail.com>
2025-09-22 18:42:47 +0530

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverExternalPackages: ["@sparticuz/chromium"],
 };
 
 export default nextConfig;
