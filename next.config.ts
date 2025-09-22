d81b0d388805d256a6328c05ddd18b76e22f383c
harkirola75way-del <harkirola.75way@gmail.com>
2025-09-22 17:15:36 +0530

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: [
+      "@sparticuz/chromium-min",
+      "playwright-core",
+    ],
+    serverlessTraceTarget: true, // ensures binaries are included
+  },
 };
 
 export default nextConfig;
